a8105ed0e304547f8b1698aa8cc63d071fc11718
Ryan Rudes <ryanrudes@gmail.com>
2021-02-22 12:27:19 -0500

    Initial Commit

---
+++ LaTeX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LaTeX
+//
+//  Created by Ryan Rudes on 2/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LaTeX/LaTeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaTeXApp.swift
+//  LaTeX
+//
+//  Created by Ryan Rudes on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct LaTeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
